023e84490c019c07d67d56401c661fc6bdde2e85
Nahidahamedridoy <nahidahamedridoy@gmail.com>
2026-04-17 23:18:32 +0600

    change Something

---
+++ src/components/shared/MyNavLink.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router';
+
+const MyNavLink = ({ to, children }) => {
+    return (
+        <NavLink
+            to={to} className={({ isActive }) =>
+                `flex items-center gap-2 ${isActive ? "text-purple-500 border-b border-purple-500" : ""}`
+            }>
+            {children}
+        </NavLink>
+    );
+};
+
+export default MyNavLink;
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BiHomeAlt2 } from 'react-icons/bi';
 import { Link, NavLink } from 'react-router';
+import MyNavLink from './MyNavLink';
+import { IoTimeOutline } from 'react-icons/io5';
+import { GoGraph } from 'react-icons/go';
 
 const Navbar = () => {
     return (
@@ -8,14 +11,14 @@ const Navbar = () => {
             <div className="flex justify-between gap-4 items-center py-[8px] container mx-auto">
                 <h1 className="font-bold text-3xl">Keen<span className='text-[#244D3F]'>Keeper</span></h1>
                 <ul className='flex justify-between gap-4 items-center'>
-                    <li>
-                        <button className='btn bg-green-800 text-white'><NavLink to={"/"}><BiHomeAlt2 />Home</NavLink></button>
+                    <li >
+                        <MyNavLink to={"/"}> <BiHomeAlt2 /> Home</MyNavLink>
                     </li>
                     <li>
-                        <NavLink to={"/timeline"}>Timeline</NavLink>
+                        <MyNavLink to={"/timeline"}><IoTimeOutline /> Timeline</MyNavLink>
                     </li>
                     <li>
-                        <NavLink to={"/states"}>Stats</NavLink>
+                        <MyNavLink to={"/states"}><GoGraph /> Stats</MyNavLink>
                     </li>
                 </ul>
             </div>
